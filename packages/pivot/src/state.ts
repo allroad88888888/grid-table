80fdeb2444b0b1b9869a139fd16b27a0ca8c188e
liqing <liuqing.yang@proinnova.com.cn>
2025-03-13 10:30:36 +0800

    fix update-version

--- packages/pivot/src/state.ts
+++ packages/pivot/src/state.ts
@@ -4,6 +4,7 @@ import type { PivotProps } from './type'
 import { formatToTable } from './format'
 import { tbodyMergeCellListAtom, theadMergeCellListAtom } from '@grid-table/view'
 import { CellFirst } from './cell/First'
+import { getValuesGroup } from './utils/const'
 
 export const dataListAtom = atom<Record<string, any>[]>([])
 
@@ -14,12 +15,12 @@ export const headerDataListAtom = atom<Record<string, any>[]>([])
 export const headerColumnListAtom = atom([])
 export const fieldsRowsAtom = atom<string[]>([])
 export const initAtom = atom(undefined, (getter, setter, { dataConfig }: PivotProps) => {
-  const { data, columns, headerData, headerMergeCellList, bodyMergeCelList } =
+  const { data, columns, headerData, headerMergeCellList, bodyMergeCelList, newColumnNameMap } =
     formatToTable(dataConfig)
   const { meta = [] } = dataConfig
   setter(dataListAtom, data)
   const rowSet = new Set(dataConfig.fields.rows)
-
+  const groupedValues = getValuesGroup(newColumnNameMap)
   setter(
     columnListAtom,
     columns.map((prop, index) => {
@@ -35,7 +36,7 @@ export const initAtom = atom(undefined, (getter, setter, { dataConfig }: PivotPr
       }
       // 格式某些cell
       meta.forEach((meItem) => {
-        if (meItem?.renderComponent) {
+        if (meItem?.renderComponent && groupedValues?.[meItem.field]?.includes(prop)) {
           columnOption.renderComponent = meItem?.renderComponent
         }
       })
